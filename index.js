8c0bad214b30c3c3210b49fe7fd5cb6b82f9219b
asaran97 <asaran234@gmail.com>
2021-02-25 17:43:03 +0530

    logging changes 10

--- index.js
+++ index.js
@@ -7,74 +7,74 @@ const asana = require("asana");
 // create task
 
 async function asanaOperations(asanaPAT, targets) {
-  try {
-    const client = asana.Client.create({
-      defaultHeaders: { "asana-enable": "new-sections,string_ids" },
-      logAsanaChangeWarnings: false
-    }).useAccessToken(asanaPAT);
-    console.log("asanaPAT", JSON.stringify(asanaPAT));
-    console.log("targets", JSON.stringify(targets));
-    // Get All project
-    client.projects
-      .getProjects({
-        data: {
-          opt_pretty: true
-        }
-      })
-      .then(result => {
-        console.log(result, "projects");
-        return result;
-      })
-      .catch(err => console.log("catch error :", JSON.stringify(err)));
+  // try {
+  const client = asana.Client.create({
+    defaultHeaders: { "asana-enable": "new-sections,string_ids" },
+    logAsanaChangeWarnings: false
+  }).useAccessToken(asanaPAT);
+  console.log("asanaPAT", JSON.stringify(asanaPAT));
+  console.log("targets", JSON.stringify(targets));
+  // Get All project
+  client.projects
+    .getProjects({
+      data: {
+        opt_pretty: true
+      }
+    })
+    .then(result => {
+      console.log(result, "projects");
+      return result;
+    })
+    .catch(err => console.log("catch error :", JSON.stringify(err)));
 
-    console.log("projects :", JSON.stringify(projects));
+  console.log("projects :", JSON.stringify(projects));
 
-    // targets.forEach(async target => {
-    //   // Get project
-    //   const targetProject = projects.find(
-    //     project => project.name.toLowerCase() === target.project.toLowerCase()
-    //   );
-    //   console.log("targetProject", JSON.stringify(targetProject));
-    //   // if(targetProject) {
+  // targets.forEach(async target => {
+  //   // Get project
+  //   const targetProject = projects.find(
+  //     project => project.name.toLowerCase() === target.project.toLowerCase()
+  //   );
+  //   console.log("targetProject", JSON.stringify(targetProject));
+  //   // if(targetProject) {
 
-    //   // }
-    // });
+  //   // }
+  // });
 
-    // client.tasks.createTask({ completed: false, pretty: true }).then(result => {
-    //   console.log("create Task result", JSON.stringify(result));
-    // });
+  // client.tasks.createTask({ completed: false, pretty: true }).then(result => {
+  //   console.log("create Task result", JSON.stringify(result));
+  // });
 
-    // // targets.forEach(async target => {
-    // let targetProject = task.projects.find(
-    //   project => project.name === target.project
-    // );
-    // if (targetProject) {
-    //   let targetSection = await client.sections
-    //     .findByProject(targetProject.gid)
-    //     .then(sections =>
-    //       sections.find(section => section.name === target.section)
-    //     );
-    //   if (targetSection) {
-    //     await client.sections.addTask(targetSection.gid, { task: taskId });
-    //     core.info(`Moved to: ${target.project}/${target.section}`);
-    //   } else {
-    //     core.error(`Asana section ${target.section} not found.`);
-    //   }
-    // } else {
-    //   core.info(`This task does not exist in "${target.project}" project`);
-    // }
-    // });
+  // // targets.forEach(async target => {
+  // let targetProject = task.projects.find(
+  //   project => project.name === target.project
+  // );
+  // if (targetProject) {
+  //   let targetSection = await client.sections
+  //     .findByProject(targetProject.gid)
+  //     .then(sections =>
+  //       sections.find(section => section.name === target.section)
+  //     );
+  //   if (targetSection) {
+  //     await client.sections.addTask(targetSection.gid, { task: taskId });
+  //     core.info(`Moved to: ${target.project}/${target.section}`);
+  //   } else {
+  //     core.error(`Asana section ${target.section} not found.`);
+  //   }
+  // } else {
+  //   core.info(`This task does not exist in "${target.project}" project`);
+  // }
+  // });
 
-    // if (taskComment) {
-    //   await client.addTask();
-    //   await client.tasks.addComment(taskId, {
-    //     text: taskComment
-    //   });
-    //   core.info("Added the pull request link to the Asana task.");
-    // }
-  } catch (ex) {
-    console.error("error :", JSON.stringify(ex));
-  }
+  // if (taskComment) {
+  //   await client.addTask();
+  //   await client.tasks.addComment(taskId, {
+  //     text: taskComment
+  //   });
+  //   core.info("Added the pull request link to the Asana task.");
+  // }
+  // } catch (ex) {
+  //   console.error("error :", JSON.stringify(ex));
+  // }
 }
 
 try {
